daaf90e881d9c73de7b6cc915727e532cad9bb3b
Leon Timmerman <leontimmerman123@gmail.com>
2020-08-27 13:16:26 +0200

    Added code for Case2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { Comp2Component } from 'src/app/comp2/comp2.component'
+import { Comp2Component } from './comp2/comp2.component'
 
 const routes: Routes = [
-  { path: 'Comp2', component: Comp2Component }
+  { path: 'comp2', component: Comp2Component }
 ];
 
 @NgModule({
