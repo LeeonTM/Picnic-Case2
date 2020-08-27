3eb1dfbc74da89abf4786682838eca5ac0de3403
Leon Timmerman <leontimmerman123@gmail.com>
2020-08-27 08:28:10 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Comp2Component } from 'src/app/comp2/comp2.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Comp2', component: Comp2Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp2Component
   ],
   imports: [
     BrowserModule,
